89213c3d29844d4b48fb83796c2d910010175033
Andrej <talevski.00@gmail.com>
2025-06-17 18:08:31 +0200

    updated the missing extensions

--- tests/pages/HomePage.ts
+++ tests/pages/HomePage.ts
@@ -1,5 +1,5 @@
 import { Page, Locator, expect } from "@playwright/test";
-import { BasePage } from "./BasePage";
+import { BasePage } from "./BasePage.ts";
 
 export class HomePage extends BasePage {
     readonly searchField: Locator;
